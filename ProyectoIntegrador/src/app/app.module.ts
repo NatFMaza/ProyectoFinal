bd1d90df2ec6f453ec1168225304852ca546c0b0
NatFMaza <flora09arg@gmail.com>
2022-10-09 11:22:41 -0300

    Dia 5
    Modificación componente redes sociales, creación botón Ingreso y personalización de portfolio

--- ProyectoIntegrador/src/app/app.module.ts
+++ ProyectoIntegrador/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { FormsModule } from '@angular/forms';
@@ -24,7 +23,6 @@ import { FooterComponent } from './components/footer/footer.component';
     EducacionComponent,
     HeaderComponent,
     LogoAPComponent,
-    SocialComponent,
     BannerComponent,
     HysComponent,
     ProyectoComponent,
